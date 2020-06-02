a4c48742c9ec386156a36c533f8c09d9cca57db0
designerofthing <designerofthings@gmail.com>
2020-06-02 14:15:44 +0200

    adds text to footer, adds css to position footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './Components/Footer';
 
 function App() {
   return (
    <div>Steve Watson <br></br> 
-     Designer of things...</div>
+     Designer of things...
+     <Footer/>
+     </div>
    
   );
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  
-
+  const name = "Steve Watson";
+  const currentDate = new Date();
+  const date = currentDate.getFullYear();
   return (
-    <div>
-      
+    <div id="footer">
+      <a id="about" href="#">
+        ABOUT
+      </a>
+      <a id="portfolio" href="#">
+        PORTFOLIO
+      </a>
+      <a id="contact" href="#">
+        CONTACT
+      </a>
+      <br></br>
+      Created by {name}.<br />
+      Copyright {date}.
     </div>
-  )
-}
+  );
+};
 
-export default Footer
-
-  
+export default Footer;
