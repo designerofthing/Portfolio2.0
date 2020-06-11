4da6fc85bc0bfddb73022ac95ea5d0689cffdad1
designerofthing <designerofthings@gmail.com>
2020-06-12 00:09:32 +0200

    adds images and links on contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Header from "./Header";
+import GitHub from '../images/Octocat.jpg'
+import LinkedIn from '../images/linkedin.png'
 
-const Contact = ({title}) => {
+const Contact = ({ title }) => {
   return (
     <div id="contact-header">
-      <Header title={title}/>
-      
+      <Header title={title} />
+      <div id="contact-grid">
+        <div className="grid-cell">
+        <img id='linkedin-icon' src={LinkedIn}/>
+          <a href='https://www.linkedin.com/in/stevewatson75aa/'>LinkedIn</a>
+        </div>
+        <div id="contact-grid">
+          <div className="grid-cell">
+          <img id='github-icon' src={GitHub}/>
+            <a href='https://github.com/designerofthing'> GitHub</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
