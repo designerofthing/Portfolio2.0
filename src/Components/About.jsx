4ce973796fc86b4acfa9bf4a6802e3331278f248
designerofthing <designerofthings@gmail.com>
2020-06-11 08:01:41 +0200

    adds grid menu layout, starts useState hooks

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Header from "./Header";
+import { useState } from "react";
 
-const About = ({title}) => {
+const About = ({ title }) => {
+  const [steve, setSteve] = useState('hidden');
+  const [skills, setSkills] = useState('hidden');
+  const [cv, setCv] = useState('hidden');
+
+  const launch = () => {
+
+  }
+
   return (
-    <div id="about-header">
-      <Header title={title} />
-      
-     
-    </div>
+    <>
+      <div id="about-header">
+        <Header title={title} />
+      </div>
+      <div id='about-grid'>
+          <div className='grid-cell'><a id="about-steve" onClick={launch(steve)}>About Steve</a></div>
+          <div className='grid-cell'><a id="about-skills" onClick={launch(skills)}>Skills</a></div>
+          <div className='grid-cell'><a id="about-cv" onClick={launch(cv)}>Curriculum Vitea</a></div>
+      </div>
+    </>
   );
 };
 
