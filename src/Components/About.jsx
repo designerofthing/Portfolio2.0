75eb5390c020483c9429c7ced6711917a76af427
designerofthing <designerofthings@gmail.com>
2020-06-12 21:00:28 +0200

    adds more platforms and ratings

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -27,8 +27,10 @@ const About = ({ title }) => {
   const programs = [
     { name: "HTML & CSS", rating: 3.5 },
     { name: "Javascript & ReactJS", rating: 3 },
+    { name: "React Native", rating: 2.5 },
     { name: "Cypress", rating: 4 },
     { name: "Ruby/Rails & RSpec", rating: 3.5 },
+    { name: "NodeJS", rating: 2 },
     { name: "Git", rating: 4 },
     { name: "Photoshop & Illustrator", rating: 3 },
     { name: "AutoCAD, Rhino, 3DStudio", rating: 3.5 },
