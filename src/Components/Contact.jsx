3649a7924320c8deeaaf1754e335d9a01fa30223
designerofthing <designerofthings@gmail.com>
2020-06-12 20:48:29 +0200

    adds images and links to contact component

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./Header";
-import GitHub from '../images/Octocat.jpg'
-import LinkedIn from '../images/linkedin.png'
+import GitHub from "../images/Octocat.jpg";
+import LinkedIn from "../images/linkedin.png";
+import Gmail from '../images/gmail.png'
 
 const Contact = ({ title }) => {
   return (
@@ -9,17 +10,23 @@ const Contact = ({ title }) => {
       <Header title={title} />
       <div id="contact-grid">
         <div className="grid-cell">
-        <img id='linkedin-icon' src={LinkedIn}/>
-          <a href='https://www.linkedin.com/in/stevewatson75aa/'>LinkedIn</a>
+          <a href="https://www.linkedin.com/in/stevewatson75aa/">
+            <img id="linkedin-icon" src={LinkedIn} alt='LinkedInLogo'/>
+            <br />
+          </a>
         </div>
-        <div id="contact-grid">
           <div className="grid-cell">
-          <img id='github-icon' src={GitHub}/>
-            <a href='https://github.com/designerofthing'> GitHub</a>
+            <a href="https://github.com/designerofthing">
+              <img id="github-icon" src={GitHub} alt='GitHubLogo'/>
+            </a>
+          </div>
+          <div className="grid-cell">
+            <a href="mailto:designerofthings@gmail.com">
+              <img id="gmail-icon" src={Gmail} alt='GmailLogo'/>
+            </a>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
