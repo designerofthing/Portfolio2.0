7161763782b8d7b054e62928fe5c1b0460847c7f
designerofthing <designerofthings@gmail.com>
2020-11-27 11:59:53 +0100

    updates projects to include gagnat

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -1,19 +1,19 @@
 const projects = [
   {
-    original: "https://portfolio2images.s3.eu-north-1.amazonaws.com/bmi.png",
-    description: "BMI App in ReactJS",
-    originalTitle: "https://reactbmi.netlify.app/",
+    original: "https://portfolio2images.s3.eu-north-1.amazonaws.com/gagnatplatform.png",
+    description: "Gagnat Platform in ReactJS",
+    originalTitle: "https://app.gagnat.se/",
+  },
+  {
+    original: "https://portfolio2images.s3.eu-north-1.amazonaws.com/gagnatcms.png",
+    description: "Gagnat CMS in WordPress",
+    originalTitle: "https://www.gagnat.se/",
   },
   {
     original:
       "https://portfolio2images.s3.eu-north-1.amazonaws.com/DailyNewsSense.png",
-    description: "News App React Client, Rails API",
+    description: "News App React Client, React Native Mobile app, Rails API",
     originalTitle: "https://dailynewssense.netlify.app/",
-  },
-  {
-    original: "https://portfolio2images.s3.eu-north-1.amazonaws.com/rps.png",
-    description: "Rock Paper Scissors App in ReactJS",
-    originalTitle: "https://steverpsgame.netlify.app/",
   },
   {
     original: "https://portfolio2images.s3.eu-north-1.amazonaws.com/request.png",
